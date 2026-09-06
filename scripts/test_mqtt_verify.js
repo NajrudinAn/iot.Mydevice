315093b854b5c2940f1bbd86a5301a46c8b20f72
Najrudin Ansari <najrudinan100@gmail.com>
2026-09-06 16:52:16 +0530

    chore: remove development scripts and temporary artifacts

--- scripts/test_mqtt_verify.js
+++
@@ -1,7 +0,0 @@
-const db = require('../backend/src/config/db');
-async function check() {
-    const res = await db.query("SELECT COUNT(*) FROM sensor_data");
-    console.log(`6B-16, 6B-17 | MQTT/Telemetry Regression | PASS | Count: ${res.rows[0].count}`);
-    process.exit(0);
-}
-check();
