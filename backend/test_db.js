531eddb1632a2c8069d3a01965f6f8e2b81fc5ef
Najrudin Ansari <najrudinan100@gmail.com>
2026-09-24 13:35:03 +0530

    Remove temporary database testing scripts

--- backend/test_db.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-const { pool } = require('./src/config/db');
-pool.query("SELECT device_id, payload, recorded_at FROM sensor_data ORDER BY recorded_at DESC LIMIT 5").then(res => { console.log(JSON.stringify(res.rows, null, 2)); pool.end(); }).catch(e => { console.error(e); pool.end(); });
